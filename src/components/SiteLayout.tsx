d0332a397b3565b96bba9bcdef9ff47c4643507b
Vladimir Kharlampidi <nolimits4web@gmail.com>
2026-05-11 19:33:42 -0400

    feat: footer

--- src/components/SiteLayout.tsx
+++ src/components/SiteLayout.tsx
@@ -1,15 +1,21 @@
 import {
+  Button,
   Link,
+  SectionTitle,
   Segmented,
   SegmentedButton,
   Toolbar,
   ToolbarButton,
+  ToolbarSeparator,
 } from '@cladd-ui/react';
 import Head from 'next/head';
 import NextLink from 'next/link';
 import type { ReactNode } from 'react';
 
 import { CladdLogo } from './CladdLogo';
+import { EnvelopeIcon } from './icons/EnvelopeIcon';
+import { GithubIcon } from './icons/GithubIcon';
+import { LogoXIcon } from './icons/LogoXIcon';
 import { MoonIcon } from './icons/MoonIcon';
 import { MoonOutlineIcon } from './icons/MoonOutlineIcon';
 import { SidebarIcon } from './icons/SidebarIcon';
@@ -80,16 +86,17 @@ export function SiteLayout({
             >
               Components
             </Link>
-            <Link
-              className="hover:opacity-75"
-              as="a"
-              href="https://github.com/cladd-ui/cladd"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </Link>
+
             <Toolbar size="sm">
+              <ToolbarButton
+                as="a"
+                href="https://github.com/cladd-ui/cladd"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GithubIcon />
+              </ToolbarButton>
+              <ToolbarSeparator />
               <ToolbarButton onClick={toggleTheme} className="sm:hidden">
                 {theme === 'light' ? <SunIcon /> : <MoonIcon />}
               </ToolbarButton>
@@ -114,7 +121,167 @@ export function SiteLayout({
         </header>
         <main className="flex-1">{children}</main>
         <footer className="border-t border-cladd-outline px-4 py-6 text-xs text-cladd-fg-soft sm:px-6">
-          Built with cladd.
+          <div className="mx-auto flex max-w-[1440px] flex-col items-start justify-start gap-8 px-4 py-12 sm:px-6">
+            <div className="xs:grid-cols-3 grid grid-cols-2 flex-wrap items-start gap-8 sm:grid-cols-4 md:flex md:gap-16">
+              <div className="flex flex-col gap-4">
+                <div className="flex items-center gap-2 text-cladd-fg">
+                  <CladdLogo className="size-8 rounded-lg border border-cladd-outline" />
+                  <span className="text-sm font-bold tracking-tight">
+                    cladd
+                  </span>
+                </div>
+                <div className="flex gap-2">
+                  <Button
+                    href="https://x.com/startpage_hq"
+                    as="a"
+                    className="cursor-pointer"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <LogoXIcon className="size-4" />
+                  </Button>
+                  <Button
+                    href="https://github.com/cladd-ui/cladd"
+                    as="a"
+                    className="cursor-pointer"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <GithubIcon className="size-4" />
+                  </Button>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4 text-sm">
+                <SectionTitle>Documentation</SectionTitle>
+                <div className="flex flex-col gap-2">
+                  <Link
+                    as={NextLink}
+                    href="/react/"
+                    className="hover:text-cladd-fg"
+                  >
+                    Introduction
+                  </Link>
+                  <Link
+                    as={NextLink}
+                    href="/react/installation/"
+                    className="hover:text-cladd-fg"
+                  >
+                    Installation
+                  </Link>
+                  <Link
+                    as={NextLink}
+                    href="/react/components/button/"
+                    className="hover:text-cladd-fg"
+                  >
+                    Components
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 text-sm">
+              <div>
+                Code licensed under{' '}
+                <Link
+                  as="a"
+                  href="https://github.com/cladd-ui/cladd/blob/master/LICENSE"
+                  target="_blank"
+                  className="underline hover:no-underline"
+                >
+                  MIT
+                </Link>
+              </div>
+              <div>
+                Copyright © {new Date().getFullYear()} Built with cladd by{' '}
+                <Link as="a" href="https://nolimits4web.com" target="_blank">
+                  <img
+                    src="/assets/products/logo-n4w.svg"
+                    alt="nolimits4web"
+                    className="inline size-6 border border-cladd-outline"
+                  />
+                </Link>
+              </div>
+              <div className="mt-2 flex items-center gap-2">
+                <a
+                  title="PaneFlow - Create Stunning Slideshows Visually. No Code Required"
+                  href="https://paneflow.com"
+                  target="_blank"
+                  className="opacity-50 grayscale hover:opacity-100 hover:grayscale-0"
+                >
+                  <img
+                    loading="lazy"
+                    src="/assets/products/logo-paneflow.svg"
+                    alt="PaneFlow - Create Stunning Slideshows Visually. No Code Required"
+                    className="inline-block size-6"
+                  />
+                </a>
+                <a
+                  title="t0ggles - Your ultimate multiple projects management tool"
+                  href="https://t0ggles.com"
+                  target="_blank"
+                  className="opacity-50 grayscale hover:opacity-100 hover:grayscale-0"
+                >
+                  <img
+                    loading="lazy"
+                    src="/assets/products/logo-t0ggles.svg"
+                    alt="t0ggles - Your ultimate multiple projects management tool"
+                    className="inline-block size-6"
+                  />
+                </a>
+                <a
+                  title="Swiper Studio - Create Beautiful And Responsive Sliders Without Writing Any Code"
+                  href="https://studio.swiperjs.com"
+                  target="_blank"
+                  className="opacity-50 grayscale hover:opacity-100 hover:grayscale-0"
+                >
+                  <img
+                    loading="lazy"
+                    src="/assets/products/logo-swiper-studio.svg"
+                    alt="Swiper Studio - Create Beautiful And Responsive Sliders Without Writing Any Code"
+                    className="inline-block size-6"
+                  />
+                </a>
+                <a
+                  title="UI Initiative - Premium templates & plugins for Swiper and Framework7"
+                  href="https://uiinitiative.com"
+                  target="_blank"
+                  className="opacity-50 grayscale hover:opacity-100 hover:grayscale-0"
+                >
+                  <img
+                    loading="lazy"
+                    src="/assets/products/logo-uiinitiative.svg"
+                    alt="UI Initiative - Premium templates & plugins for Swiper and Framework7"
+                    className="inline-block size-6"
+                  />
+                </a>
+                <a
+                  title="Start Page HQ"
+                  href="https://startpagehq.com"
+                  target="_blank"
+                  className="opacity-50 grayscale-100 hover:opacity-100 hover:grayscale-0"
+                >
+                  <img
+                    loading="lazy"
+                    src="/assets/products/logo-startpagehq.svg"
+                    alt="Start Page HQ"
+                    className="inline-block size-6"
+                  />
+                </a>
+                <a
+                  title="Fisper - Local AI Voice Dictation for macOS"
+                  href="https://fisper.app"
+                  target="_blank"
+                  className="opacity-50 grayscale hover:opacity-100 hover:grayscale-0"
+                >
+                  <img
+                    loading="lazy"
+                    src="/assets/products/logo-fisper.png"
+                    alt="Fisper - Local AI Voice Dictation for macOS"
+                    className="inline-block size-6"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
         </footer>
       </div>
     </SidebarProvider>
